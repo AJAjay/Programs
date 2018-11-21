9f858bbf2003024e9821cbb6dcf2fd3933b06c17
AJAjay <ajajay271@gmail.com>
2018-11-21 11:02:58 +0530

    send email using python

--- EmailTest.py
+++ EmailTest.py
@@ -1,17 +1,23 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+# construct the message in particular format
 msg = MIMEMultipart()
-msg['From'] = "ajajay271@gmail.com"
-msg['To'] = "ajajaykmr@gmail.com"
-msg['Subject'] = "TestMail"
+msg['From'] = "from --> mail id"
+msg['To'] = "to --> mail id"
+msg['Subject'] = "subject of the mail"
 
-body = "<a href=""https://www.google.com/""> click to show google </a>"
+body = "<a href=""https://www.google.com/""> click to show google </a> "
+#attach the body of the message with specify the format
 msg.attach(MIMEText(body,'html'))
-print(msg)
-password = "Ajay@271"
+#print(msg)
+#add your password
+password = "your password"
+#define the smtp server here it is gmail
 server = smtplib.SMTP("smtp.gmail.com", 587)
+#start the server Transport layer security-- starttls() is ESMTP function to start ssl connection between two server 
 server.starttls()
+#login with email id and password
 server.login(msg['From'], password)
 server.sendmail(msg['From'], msg['To'], msg.as_string())
 server.quit()
